9bb4572f5f69d0cab744db47557c7bb9a56a6638
liuguicen <2583657917@qq.com>
2018-04-16 21:09:02 +0800

    multi modifies

--- memory-note/src/main/java/com/lgc/memorynote/wordDetail/WordDetailActivity.java
+++ memory-note/src/main/java/com/lgc/memorynote/wordDetail/WordDetailActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
